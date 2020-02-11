8bc82f389d16289c51f09fb9bd7f7c57d3d11e2e
Cyril <cyril.vuillemin@hotmail.fr>
2020-02-11 10:12:38 +0100

    init commit

---
+++ src/main/java/com/LC/LCspringfev/LcSpringFevApplication.java
@@ -0,0 +1,13 @@
+package com.LC.LCspringfev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcSpringFevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcSpringFevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LC/LCspringfev/LcSpringFevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LC.LCspringfev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcSpringFevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
